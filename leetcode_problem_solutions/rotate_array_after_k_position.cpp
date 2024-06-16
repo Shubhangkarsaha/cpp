34e8a893fb59592184cffa05851e813642eacbed
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-06-16 15:39:26 +0530

    Create rotate_array_after_k_position.cpp
    shift by k th position in cyclic way.

---
+++ leetcode_problem_solutions/rotate_array_after_k_position.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        vector<int> temp(nums.size());
+        for (int i=0;i<nums.size();i++) {
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        nums = temp;
+    }
+};
