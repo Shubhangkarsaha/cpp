c433b87b3e3e36088b7cb75765a970db97ee8b94
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-06-26 22:27:49 +0530

    rotate at k th index using search function

--- pivot_element/pivot.cpp
+++ pivot_element/pivot.cpp
@@ -44,7 +44,16 @@ int binary_search (vector<int>& arr, int s, int e, int key) {
     }
     return -1;
 }
-
+//searching and rotate the element at k th index
+int search(vector<int>& arr, int n, int k)
+{
+    int pivot = getpivot(arr, n);
+    if (k >= arr[pivot] && k <= arr[n - 1]) {
+        return binary_search(arr, pivot, n-1, k);
+    } else {
+        return binary_search(arr, 0, pivot-1, k);
+    }
+}
 
 int main () {
 
