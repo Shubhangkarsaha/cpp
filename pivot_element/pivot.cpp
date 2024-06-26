452653bd2ed27d08ff1be5232412e64fd12e3a7a
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-06-26 22:24:51 +0530

    getting pivot element by pivot function

--- pivot_element/pivot.cpp
+++ pivot_element/pivot.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+//getting the pivot element
+int getpivot (vector<int>& arr, int size) {
+
+    int s = 0;
+    int e = size - 1;
+    int mid = s + (e-s)/2;
+
+    while (s < e) {
+
+        if (arr[mid] >= arr[0]) {
+            s = mid + 1;
+        } else {
+            e = mid;
+        }
+        mid = s + (e-s)/2;  
+    }
+    return s;
+}
+
+
+
 int main () {
 
     vector<int> arr;
