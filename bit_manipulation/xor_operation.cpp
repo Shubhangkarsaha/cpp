7339169e0444bd447a3acaf83083d1d7ba0a1a3f
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-07-22 22:45:51 +0530

    Update xor_operation.cpp

--- bit_manipulation/xor_operation.cpp
+++ bit_manipulation/xor_operation.cpp
@@ -22,19 +22,19 @@ int findDuplicate (int arr[], int size) {
 
 int main () {
 
-    // int size;
-    // cout << "Enter the Size of array:" << endl;
+    int size;
+    cout << "Enter the Size of array:" << endl;
 
-    // cin >> size;
+    cin >> size;
 
-    // int arr[size];
+    int arr[size];
 
-    // cout << "Enter the element of array:" << endl;
-    // for (int i=0; i<size; i++) {
-    //     cin >> arr[i];
-    // }
+    cout << "Enter the element of array:" << endl;
+    for (int i=0; i<size; i++) {
+        cin >> arr[i];
+    }
 
-    // cout << findDuplicate (arr, size) << endl;
+    cout << findDuplicate (arr, size) << endl;
 
     int a = 7 ^ 8;
 
