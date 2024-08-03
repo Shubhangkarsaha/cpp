a2f895c2315cfa9f69844601eb98916579c53838
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-08-03 14:02:49 +0530

    merge-sort-optimized-approach in O(n) time complexity and constant space complexity

---
+++ Advance-data-Structure/linked-list/merge-sort-in-linked-list.cpp
@@ -0,0 +1,95 @@
+/********************************************************************
+
+    Following is the representation of the Singly Linked List Node:
+
+    class node{
+        public:
+            int data;
+            node * next;
+            node(int data){
+                this->data=data;
+                this->next=NULL;
+            }
+    };
+    
+********************************************************************/
+
+//find the mid element of the linked list
+node* findMid (node* head) {
+
+    node* slow = head;
+    node* fast = head -> next;
+
+    while (fast != NULL && fast -> next != NULL) {
+
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    } 
+    return slow;
+} 
+
+//merge 2 sorted linked list
+node* merge (node* left, node* right) {
+
+    if (left == NULL) {
+        return right;
+    }
+
+    if (right == NULL) {
+        return left;
+    }
+
+    node* ans = new node(-1);
+    node* temp = ans;
+
+    while (left != NULL && right != NULL) {
+        if (left -> data < right -> data) {
+            temp -> next = left;
+            temp = left;
+            left = left -> next;
+        } else {
+            temp -> next = right;
+            temp = right;
+            right = right -> next;
+        }
+    }
+
+    while (left != NULL) {
+        temp -> next = left;
+        temp = left;
+        left = left -> next;
+    }
+
+    while (right != NULL) {
+        temp -> next = right;
+        temp = right;
+        right = right -> next;
+    }
+
+    ans = ans -> next;
+    return ans;
+}
+
+node* mergeSort(node *head) {
+   
+    //base case
+    if (head == NULL || head -> next == NULL) {
+        return head;
+    }
+
+    //split linked list into two halves after finding mid
+    node* mid = findMid (head);
+
+    node* left = head;
+    node* right = mid -> next;
+    mid -> next = NULL;
+
+    //recursive calls to sort both halves
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    //merge both left and right
+    node* result = merge(left, right);
+
+    return result;
+}
