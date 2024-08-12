db07a50126da6df23502c4565bae623ba5293a12
Shubhangkar Saha <107031569+Shubhangkarsaha@users.noreply.github.com>
2024-08-12 23:47:24 +0530

    Delete-middle-element-from-stack

--- Advance-data-Structure/Stack/Delete-middle-element.cpp
+++ Advance-data-Structure/Stack/Delete-middle-element.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h> 
 
+// the following question is solved by tail recursive solution
+
 void StackTraverse (stack<int>& inputStack, int count, int N) {
 
    //If stack is empty
